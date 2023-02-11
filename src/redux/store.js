5995937f8164e24d8a394c8a5c954e737caafdf5
Piyush <bhambhanipiyush4@gmail.com>
2023-02-11 18:33:43 +0530

    Added middleware logger

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,11 @@
 import { legacy_createStore , applyMiddleware} from "redux";
-
+import logger from 'redux-logger'
 // notice here now we are using the es6 syntax
 // now instead of importing cake reducer just import rootreducer
 // and attach the reducer to store :)
 import rootReducer from "./rootReducer";
 
-const store = legacy_createStore(rootReducer);
+const store = legacy_createStore(rootReducer,applyMiddleware(logger));
 export default store
 
 // now we have created a store
