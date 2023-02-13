d752be995ae7dd00e5c7fa06e2737febb4b09be1
Piyush <bhambhanipiyush4@gmail.com>
2023-02-13 10:31:41 +0530

    change reducer for payload caluculation

--- src/redux/cake/cakeReducer.js
+++ src/redux/cake/cakeReducer.js
@@ -6,11 +6,11 @@ const initialState={
     numOfCakes:10
 }
 
-const cakeReducer=(state=initialState,action)=>{
+const cakeReducer=(state=initialState,action)=>{ // modifying as we have action payload to work
     switch(action.type){
         case  BUY_CAKE:
             return { ...state,
-                numOfCakes: state.numOfCakes-1
+                numOfCakes: state.numOfCakes-action.payload
             }
             default :
                 return state
