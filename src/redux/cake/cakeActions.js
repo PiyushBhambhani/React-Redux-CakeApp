0eb8e68d15b8b21ec3bd573c951951769511b553
Piyush <bhambhanipiyush4@gmail.com>
2023-02-13 10:31:00 +0530

    Add num default arg in action payload

--- src/redux/cake/cakeActions.js
+++ src/redux/cake/cakeActions.js
@@ -1,8 +1,9 @@
 import  {BUY_CAKE}  from "./cakeTypes"
 
-export const buyCake =()=>{
+export const buyCake =(number=1)=>{ //so now it takes number by default we have value 1, so it retains the past functionality
     return{
-        type: BUY_CAKE
+        type: BUY_CAKE,
+        payload: number
     }
 }
 // export default buyCake; doesn't work here due to default
