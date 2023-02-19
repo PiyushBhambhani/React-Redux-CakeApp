4bbeec9fdedea7cb9cbbfb16b2fb3a7376eda75b
Piyush <bhambhanipiyush4@gmail.com>
2023-02-19 11:12:24 +0530

    exported userAction creators index

--- src/redux/index.js
+++ src/redux/index.js
@@ -1,3 +1,6 @@
 export { buyCake } from './cake/cakeActions'
 export {buyIceCream } from './iceCream/iceCreamActions'
 // here we'll export  all the actionCreators 
+
+export * from "./user/userActions"
+//next/final step is to include reducer into  combinereducer (rootred)
